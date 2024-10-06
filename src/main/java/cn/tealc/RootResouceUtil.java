9f994a86e548f9d1c5cedf62613a05c9a56c5d3a
Leck995 <yiqiu995@gmail.com>
2024-10-06 17:09:06 +0800

    初步提交

---
+++ src/main/java/cn/tealc/RootResouceUtil.java
@@ -0,0 +1,53 @@
+package cn.tealc;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.tealc.model.Resource;
+import cn.tealc.model.RootResource;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @program: WutheringWavesToolResources
+ * @description:
+ * @author: Leck
+ * @create: 2024-10-06 15:54
+ */
+public class RootResouceUtil {
+    public static void main(String[] args) throws IOException {
+        init();
+    }
+    public static void init() throws IOException{
+        String version = "1.0.0";
+        String aimDir="assets/data/%s";
+
+
+        RootResource rootResource = new RootResource();
+        Map<String, Resource> map = new LinkedHashMap<>();
+        File dir =new File("data");
+        File[] jsons = dir.listFiles();
+        for (File json : jsons) {
+            String suffix = FileUtil.getSuffix(json);
+            if (suffix.equals("json")){
+                String md5 = DigestUtil.md5Hex(json);
+                String name = FileUtil.mainName(json);
+
+                String filename = json.getName();
+                String aimPath = String.format(aimDir, filename);
+                map.put(name,new Resource(json.getName(),filename,aimPath,md5));
+            }else {
+                System.err.println(json.getName()+"非JSON文件");
+            }
+        }
+
+        rootResource.setVersion(version);
+        rootResource.setResources(map);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File("data/Root.json"),rootResource);
+    }
+}
---
+++ src/main/java/cn/tealc/model/RoleName.java
@@ -0,0 +1,12 @@
+package cn.tealc.model;
+
+/**
+ * @program: WutheringWavesToolResources
+ * @description:
+ * @author: Leck
+ * @create: 2024-10-06 15:57
+ */
+public class RoleName {
+    private String roleName;
+
+}
