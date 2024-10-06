ec471510ca1a4129079f3beb86777d590cd88113
Leck995 <yiqiu995@gmail.com>
2024-10-07 00:48:47 +0800

    更新结构

---
+++ src/main/java/cn/tealc/model/weight/PhantomWeight.java
@@ -0,0 +1,37 @@
+package cn.tealc.model.weight;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Map;
+
+/**
+ * @program: WutheringWavesTool
+ * @description:
+ * @author: Leck
+ * @create: 2024-10-02 23:08
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PhantomWeight {
+    private String roleName;
+    private Map<String,Integer> subPropWeights;
+
+
+
+
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Map<String, Integer> getSubPropWeights() {
+        return subPropWeights;
+    }
+
+    public void setSubPropWeights(Map<String, Integer> subPropWeights) {
+        this.subPropWeights = subPropWeights;
+    }
+}
